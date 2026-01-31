92ed8466d6687ea6ceecda80130c035feb47be96
Pekka Heljakka <pekka.heljakka@tuni.fi>
2026-01-31 20:17:16 +0200

    Tweak mask colors

--- MaskData/mask_properties.gd
+++ MaskData/mask_properties.gd
@@ -2,7 +2,7 @@ class_name MaskProperties
 extends Node
 
 enum PropType {EMPTY, EYES, EARS, NOSE, MOUTH, HORNS, HAT, HAIR}
-enum PropColor {NO, RED, BLUE, GREEN}
+enum PropColor {NO, RED, BLUE, WHITE}
 
 const MASK_SPRITES : Array = [
 "res://MaskData/Sprites/Props/maskbase1.png",
@@ -48,6 +48,6 @@ const HAT_SPRITES : Array = ["res://MaskData/Sprites/Hat.png"]
 const HAIR_SPRITES : Array = ["res://MaskData/Sprites/Hair.png"]
 
 const PROP_COLOR_DICT: Dictionary = {
-PropColor.RED : Color.RED,
-PropColor.BLUE : Color.BLUE,
-PropColor.GREEN : Color.GREEN}
+PropColor.RED : Color.DARK_RED,
+PropColor.BLUE : Color.MIDNIGHT_BLUE,
+PropColor.WHITE : Color.WHITE}
