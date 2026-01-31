5af37da22f832aa042817e3065997e73cb74dc4d
Elias Vähäjylkkä <vahajylkkaelias@gmail.com>
2026-01-31 19:01:24 +0200

    better buttons

--- button.gd
+++ button.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
 signal pressed
-var orig_scale: Vector2
+var orig_pos: Vector2
 var can_press = true
 
 func _ready() -> void:
-	orig_scale = $Sprite.scale
+	orig_pos = $Sprite.position
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed && can_press):
 		pressed.emit()
-		$Sprite.scale = .9 * orig_scale
+		$Sprite.position.y += 20
 		await get_tree().create_timer(0.1).timeout
-		$Sprite.scale = orig_scale
+		$Sprite.position = orig_pos
